facf20a9fc489af33a586cdd26aea98a757b4d74
Pherserk <michelecarino84@gmail.com>
2016-10-09 12:58:42 +0200

    Listings tokenization

--- Tests/Components/ContentBlockTokenizerTest.php
+++ Tests/Components/ContentBlockTokenizerTest.php
@@ -13,7 +13,7 @@ class ContentBlockTokenizerTest extends BaseUnitTestCase
      */
     public function testTokenize($content, $tokenizedContent)
     {
-        $tokenizer = new ContentBlockTokenizer(['div', 'p'], ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']);
+        $tokenizer = new ContentBlockTokenizer(['div', 'p'], [['ul', 'li'], ['ol', 'li'], ['table', 'tr']], ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']);
 
         self::assertEquals($tokenizedContent, $tokenizer->tokenize($content));
     }
@@ -24,6 +24,7 @@ class ContentBlockTokenizerTest extends BaseUnitTestCase
             ['<div id="the-div"><h1>The div content</h1></div>', '[!#!BLOCK!#!][!#!BLOCK!#!][!#!HEAD!#!]The div content[#!#HEAD#!#][#!#BLOCK#!#][#!#BLOCK#!#]'],
             ['<div id="the-div">The div content</div>', '[!#!BLOCK!#!]The div content[#!#BLOCK#!#]'],
             ['<p id="the-paragraph">The paragraph content</p>', '[!#!BLOCK!#!]The paragraph content[#!#BLOCK#!#]'],
+            ['<ol id="the-ordered-list"><li>Item 1</li><li>Item 2</li></ol>', '[!#!BLOCK!#!][!#!LIST!#!][!#!ITEM!#!]Item 1[#!#ITEM#!#][!#!ITEM!#!]Item 2[#!#ITEM#!#][#!#LIST#!#][#!#BLOCK#!#]'],
         ];
     }
 }
--- src/Components/ContentBlockTokenizer.php
+++ src/Components/ContentBlockTokenizer.php
@@ -5,38 +5,61 @@ namespace Components;
 
 class ContentBlockTokenizer
 {
-    const OPENING_TOKEN = '[!#!BLOCK!#!]';
-    const CLOSING_TOKEN = '[#!#BLOCK#!#]';
+    const OPENING_BLOCK = '[!#!BLOCK!#!]';
+    const CLOSING_BLOCK = '[#!#BLOCK#!#]';
     const OPENING_HEADING = '[!#!HEAD!#!]';
     const CLOSING_HEADING = '[#!#HEAD#!#]';
+    const OPENING_LIST = '[!#!LIST!#!]';
+    const CLOSING_LIST = '[#!#LIST#!#]';
+    const OPENING_ITEM = '[!#!ITEM!#!]';
+    const CLOSING_ITEM = '[#!#ITEM#!#]';
 
 
     /** @var string[] */
     private $blockTags;
+    /** @var string[] */
+    private $listTags;
+    /** @var string[] */
+    private $headingTags;
 
-    public function __construct(array $blockTags, array $headingTags)
+    public function __construct(array $blockTags, array $listTags, array $headingTags)
     {
         $this->headingTags = $headingTags;
+        $this->listTags = $listTags;
         $this->blockTags = $blockTags;
     }
 
 
     public function tokenize($htmlContent)
     {
-        foreach($this->headingTags as $headingTag) {
-            $openingRegex = sprintf('/<%s[^>]*>/is', $headingTag);
-            $closingRegex = sprintf('/<\/%s>/is', $headingTag);
-
-            $htmlContent = preg_replace($openingRegex, self::OPENING_TOKEN . self::OPENING_HEADING, $htmlContent);
-            $htmlContent = preg_replace($closingRegex, self::CLOSING_HEADING . self::CLOSING_TOKEN, $htmlContent);
-        }
-
         foreach($this->blockTags as $blockTag) {
             $openingRegex = sprintf('/<%s[^>]*>/is', $blockTag);
             $closingRegex = sprintf('/<\/%s>/is', $blockTag);
 
-            $htmlContent = preg_replace($openingRegex, self::OPENING_TOKEN, $htmlContent);
-            $htmlContent = preg_replace($closingRegex, self::CLOSING_TOKEN, $htmlContent);
+            $htmlContent = preg_replace($openingRegex, self::OPENING_BLOCK, $htmlContent);
+            $htmlContent = preg_replace($closingRegex, self::CLOSING_BLOCK, $htmlContent);
+        }
+
+        foreach($this->listTags as $listTag) {
+            $openingRegex = sprintf('/<%s[^>]*>/is', $listTag[0]);
+            $closingRegex = sprintf('/<\/%s>/is', $listTag[0]);
+
+            $htmlContent = preg_replace($openingRegex, self::OPENING_BLOCK . self::OPENING_LIST, $htmlContent);
+            $htmlContent = preg_replace($closingRegex, self::CLOSING_LIST . self::CLOSING_BLOCK, $htmlContent);
+
+            $openingItemRegex = sprintf('/<%s[^>]*>/is', $listTag[1]);
+            $closingItemRegex = sprintf('/<\/%s>/is', $listTag[1]);
+
+            $htmlContent = preg_replace($openingItemRegex, self::OPENING_ITEM, $htmlContent);
+            $htmlContent = preg_replace($closingItemRegex, self::CLOSING_ITEM, $htmlContent);
+        }
+
+        foreach($this->headingTags as $headingTag) {
+            $openingRegex = sprintf('/<%s[^>]*>/is', $headingTag);
+            $closingRegex = sprintf('/<\/%s>/is', $headingTag);
+
+            $htmlContent = preg_replace($openingRegex, self::OPENING_BLOCK . self::OPENING_HEADING, $htmlContent);
+            $htmlContent = preg_replace($closingRegex, self::CLOSING_HEADING . self::CLOSING_BLOCK, $htmlContent);
         }
 
         return $htmlContent;
